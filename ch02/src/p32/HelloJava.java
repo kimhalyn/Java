9d212d929b2e4c513c0677288276cb0134e47f1f
khl <reedweed@naver.com>
2020-11-02 09:02:00 +0000

    ch02 수업예제

---
+++ ch02/src/p32/HelloJava.java
@@ -0,0 +1,16 @@
+package p32;
+/**
+ * 날짜 : 2020/11/02
+ * 이름 : 김하린
+ * 내용 : 교제 p32 따라하기
+ * 
+ */
+public class HelloJava {
+
+	public static void main(String[] args) {
+		
+	// System.out.println("Hello Java");
+		
+		System.out.println("안녕 자바");
+	}
+}
---
+++ ch02/src/sub1/HelloJava.java
@@ -0,0 +1,13 @@
+package sub1;
+
+/**
+ * 날짜 : 2020/11/02
+ * 이름 : 김하린
+ * 내용 : ch02. 자바 설치 예제 교재 p31
+ */
+public class HelloJava {
+
+	public static void main(String[] args) {
+	System.out.println("Hello Java");
+}
+}
