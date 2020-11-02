ab18e5a79483b7882137c27bf4695c68080adc48
khl <reedweed@naver.com>
2020-11-02 09:55:35 +0000

    ch01 수업예제

---
+++ ch01/src/sub1/HelloWorld.java
@@ -0,0 +1,15 @@
+package sub1;
+
+/*
+ * 날짜 : 2020/11/02
+ * 이름 : 김하린
+ * 내용 : ch01 자바 HelloWorld 출력하기
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");
+		
+	}
+}
